2269f1fa1c683ae834b62c1d4f023dc5816376e2
Uchenna Adubasim <uchadubs@yahoo.com>
2025-08-07 20:40:22 +0100

    added success url

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CustomUserCreationView
+from .views import CustomUserCreationView, SuccessView
 
 urlpatterns = [
     path('home/', CustomUserCreationView.as_view(), name='home'),
+    path('success/', SuccessView.as_view(), name='success')
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from .models import CustomUser
 from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
 
 
 class CustomUserCreationView(CreateView):
     model = CustomUser
     form_class = CustomUserCreationForm
     template_name = 'home.html'
+    success_url = reverse_lazy('success')
+
+class SuccessView(TemplateView):
+    template_name = 'success.html'
